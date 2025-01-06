62ac37de18e3fff7014cfe230b65a7e385d322db
lukafblepkowski <lukalepkowski@gmail.com>
2025-01-06 16:46:46 +0000

    Item CSV system

---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+    <head>
+        <title>spunky2</title>
+        <link rel='stylesheet' href='style.css'>
+    </head> 
+    <body>
+        <span id='cB'> Welcome to spunky2.com</span>
+        <i id='cM'>(Luka FB Lepkowski's personal repo / website)</i>
+
+        <br/><br/><u id='cG'>Things I've made</u><br/><br/>
+        
+        <?php
+
+        $file = fopen('items.csv', 'r');
+
+        if($file !== false) {
+            while(($data = fgetcsv($file)) !== false) {
+                $title = $data[0];
+                $img_addr = $data[1];
+                $release = $data[2];
+                $tag = $data[3];
+                $desc = $data[4];
+                $rawhtml_source = $data[5];
+                $source = $data[6];
+                $rawhtml_download = $data[7];
+                $download = $data[8];
+
+                echo "
+                <div id='item'>
+                    <img id='icon' src='",$img_addr,"'>
+                    <div id='inline'>
+                        <span id='cB'>",$title,"    </span><i id='cBD'> (Released ",$release,")</i><br/>
+                        <i id='cMD'>",$tag,"</i><br/><br/>
+                        <p id='cDesc'>",$desc,"</p><br/>
+                        <div id='footer'>
+                            <span id='cC'>Source code: </span><a id=cY href=",$rawhtml_source,">",$source,"</a><br/>
+                            <span id='cCW'>Info / Download page: </span><a id=cY href=",$rawhtml_download,">",$download,"</a>
+                        </div>
+                    </div>
+                </div>
+                
+                ";
+            }
+
+            $fclose($file);
+        }
+        
+        
+        ?>
+
+        
+    
+    </body>
+</html>
